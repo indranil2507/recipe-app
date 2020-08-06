a585cea265d64b17893347f7e5b17ee9158b4f83
indranil2507 <iindranilsaha1@gmail.com>
2020-08-06 22:51:42 +0530

    Updates

---
+++ precache-manifest.d437b5d0868baaf58557683d851c5aa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f47e378b6c012df7e3926bfee363bbc",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "1b4c7620008ece2cd9d8",
+    "url": "/recipe-app/static/css/main.7cdd76b9.chunk.css"
+  },
+  {
+    "revision": "cf98864d1891d8882354",
+    "url": "/recipe-app/static/js/2.56954e41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.56954e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4c7620008ece2cd9d8",
+    "url": "/recipe-app/static/js/main.711327cb.chunk.js"
+  },
+  {
+    "revision": "1f2b12c6ca57b65844eb",
+    "url": "/recipe-app/static/js/runtime-main.1141f473.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
